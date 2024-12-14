9519a3877af7f954aa43d2d81ee8ce991b0665b8
bpsk <phonepyaesonekhant586@gmail.com>
2024-12-14 22:30:34 +0630

    adding apt service

--- src/main/java/com/DoctorAppointment/controller/AppointmentController.java
+++ src/main/java/com/DoctorAppointment/controller/AppointmentController.java
@@ -1,7 +1,12 @@
 package com.DoctorAppointment.controller;
 
+import com.DoctorAppointment.service.AppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class AppointmentController {
+
+    @Autowired
+    private AppointmentService aptSer;
 }
